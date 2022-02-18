4e7faea41734a72d763f6df805c8224bd0dbd6e8
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-02-18 12:32:15 +0100

    Declaring all function for the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { ethers } from "ethers";
 import './App.css';
 
 function App() {
+
+  const [loader, setLoader] = useState(true);
+  const [accounts, setAccounts] = useState([]);
+  const [balance, setBalance] = useState();
+  const [succes, setSucces] = useState('');
+  const [error, setError] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
