0caa4398bbfc75bdca3155292074b31d3f0d9d2c
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-02-18 12:50:33 +0100

    Set function Reload, Connect , Changed Chain, and disconnect

--- src/App.js
+++ src/App.js
@@ -12,18 +12,41 @@ function App() {
 
   useEffect(() => {
     getAccounts();
+    setLoader(false);
   }, [])
+
+  window.ethereum.addListener('connect', async(response) =>{
+    getAccounts();
+    console.log('ok');
+  })
+
+  window.ethereum.on('accountsChanged', () => {
+    window.location.reload();
+  })
+
+  window.ethereum.on('chainChanged', () =>{
+    window.location.reload();
+  })
+
+  window.ethereum.on('disconnect', () =>{
+    window.location.reload();
+  })
 
   async function getAccounts() {
     if(typeof window.ethereum !== 'undefined') {
-      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
-      console.log(accounts);
+      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts'}); 
+      setAccounts(accounts);
     }
   }
 
   return (
     <div className="App">
-
+      {!loader &&
+       accounts.length > 0 ?
+       <p>You are connect with this account : {accounts[0]}</p>
+       :
+       <p>You are not connected with Metamask to this website.</p>
+     }
     </div>
   );
 }
